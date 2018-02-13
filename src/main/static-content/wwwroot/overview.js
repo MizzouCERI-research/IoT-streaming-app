814d073eb6d32e138002fb7abd8b350348862ccb
wangso <wangso@missouri.edu>
2018-02-13 00:15:42 -0600

    hard coding of data variable is working, need to make updateData work

--- src/main/static-content/wwwroot/overview.js
+++ src/main/static-content/wwwroot/overview.js
@@ -1,6 +1,7 @@
 
-window.onload = function () {
 
+window.onload= function(){
+	
 	var chart = new CanvasJS.Chart("chartContainer", {
 		title: {
 			text: "Overview of Emotion Measurements"
@@ -9,166 +10,103 @@ window.onload = function () {
 			title: "Emotion level",
 			suffix: ""
 		},
-		data:[
-			{
-			type: "column",	
-			showInLegend: true,
-			name: "user1",
-			color: "Blue",
-			yValueFormatString: "0.##",
-			indexLabel: "{y}",
-			dataPoints: [
-				{ label: "Engagement", y: 0.80 },
-				{ label: "Focus", y: 0.80 },
-				{ label: "Excitement", y: 0.80 },
-				{ label: "Frustration", y: 0.40 },
-				{ label: "Stress", y: 0.40 },
-				{ label: "Relaxation", y: 0.80 }
-			]					
-		}]
-	});
-	console.log("I am here 1");
-
-	function updateChart () {
-	//		var barColor, yVal;
-			var dps = new Array(6);		
-			var measurements = ["Engagement","Focus","Excitement","Frustration","Stress","Relaxation"];		
-			console.log("I am here 2");
+		data:[]
+	});		
 	
-	//    	str = JSON.stringify(data);
-	//        console.log(str);
-	//        alert(str);
-			
-			for (var i=0; i< numUsers; i++){
-	//			dps[i] = chart.options.data[i].dataPoints;
-				var name = chart.options.data[i].name;
-				
-				for (var j=0; j< numUsers; j++){
-					if (data[j].name == name) {
-						chart.options.data[i].dataPoints = data[j].userData;
-					}				
-				}
-				
-	//			for (var j = 0; j < 6; j++) {
-	//								
-	//				chart.options.data[i].dataPoints[j] = data[name][j];
-	//				
-	//				
-	//				
-	//				
-	////		        if (yVal <= 0.0){
-	////		        	yVal=0.01;
-	////		        }
-	////		        if (yVal >= 1.0){
-	////		        	yVal=0.99;
-	////		        }				
-	////				barColor = yVal > 0.8 ? "Red" : yVal >= 0.5 ? "Yellow" : yVal < 0.5 ? "Green" : null;
-	//				
-	//		        if (j==0 || j==1 || j==2 || j==5){
-	//		        	barColor = yVal <0.7? "Red": chart.options.data[i].color;
-	//		        }
-	//		        else {
-	//		        	barColor = yVal > 0.5 ? "Red": chart.options.data[i].color;
-	//		        }
-	//				dps[i][j] = {label: measurements[j] , y: yVal, color: barColor};
-	//			}
-	//			chart.options.data[i].dataPoints = dps[i]; 
-			}
-			chart.render();
-			data = [];
+	data=[{"name":"user1","userData":[{"label":"engagement","y":0.88},{"label":"focus","y":0.816},{"label":"excitement","y":0.713},{"label":"frustration","y":0.293},{"label":"stress","y":0.175},{"label":"relaxation","y":0.512}]},
+		{"name":"user2","userData":[{"label":"engagement","y":0.883},{"label":"focus","y":0.776},{"label":"excitement","y":0.669},{"label":"frustration","y":0.32},{"label":"stress","y":0.2},{"label":"relaxation","y":0.478}]},
+			{"name":"user3","userData":[{"label":"engagement","y":0.883},{"label":"focus","y":0.776},{"label":"excitement","y":0.669},{"label":"frustration","y":0.32},{"label":"stress","y":0.2},{"label":"relaxation","y":0.478}]}];
+ 
+	updateNumBars(numUsers, chart);
 
+	//data = updateData(resource, secondsAgo);
+	
+	updateChart(chart, data);
+	
+	setInterval(function() {updateChart(chart, data)}, 1000);
+
+}//window.onload closing parenthesis
+
+
+
+var updateNumBars = function (num, paramChart){
+	for (var i=1; i<=num; i++) {
+		if(i % 2 ==0){
+			paramChart.options.data.push(
+				{
+					type: "column",	
+					showInLegend: true,
+					name: "user"+i,
+					color: "Blue",
+					yValueFormatString: "0.##",
+					indexLabel: "{y}",
+					dataPoints: [
+						{ label: "Engagement", y: 0.80 },
+						{ label: "Focus", y: 0.80 },
+						{ label: "Excitement", y: 0.80 },
+						{ label: "Frustration", y: 0.40 },
+						{ label: "Stress", y: 0.40 },
+						{ label: "Relaxation", y: 0.80 }
+					]					
+				}			
+			);
+		}else{
+			paramChart.options.data.push(
+				{
+					type: "column",	
+					showInLegend: true,
+					name: "user"+i,
+					color: "Green",
+					yValueFormatString: "0.##",
+					indexLabel: "{y}",
+					dataPoints: [
+						{ label: "Engagement", y: 0.80 },
+						{ label: "Focus", y: 0.80 },
+						{ label: "Excitement", y: 0.80 },
+						{ label: "Frustration", y: 0.40 },
+						{ label: "Stress", y: 0.40 },
+						{ label: "Relaxation", y: 0.80 }
+					]					
+				}			
+			);
+		}	
 	};		
+};
 
-	updateChart();
+var updateData = function(resource, secondsAgo) {
 	
-var UIHelper = function(data, chart) {
+	var localData;
+    // Fetch data from our data provider
+    provider.getData(resource, secondsAgo, function(newData) {
+      // Store the data locally
+    	dataAll.addNewData(newData);
+    	localData = dataAll.getData();
+      	str = JSON.stringify(localData);
+        console.log(str);
+        alert(str); 
+    });
+    return localData;
+}
 
-	var	updateUserNum = function (){
-					
-		for (var i=0; i<numUsers; i++) {
-			if(i % 2 ==0){
-				chart.options.data.push(
-					{
-						type: "column",	
-						showInLegend: true,
-						name: "user"+i,
-						color: "Blue",
-						yValueFormatString: "0.##",
-						indexLabel: "{y}",
-						dataPoints: [
-							{ label: "Engagement", y: 0.80 },
-							{ label: "Focus", y: 0.80 },
-							{ label: "Excitement", y: 0.80 },
-							{ label: "Frustration", y: 0.40 },
-							{ label: "Stress", y: 0.40 },
-							{ label: "Relaxation", y: 0.80 }
-						]					
-					}			
-				);
-			}else{
-				chart.options.data.push(
-					{
-						type: "column",	
-						showInLegend: true,
-						name: "user"+i,
-						color: "Green",
-						yValueFormatString: "0.##",
-						indexLabel: "{y}",
-						dataPoints: [
-							{ label: "Engagement", y: 0.80 },
-							{ label: "Focus", y: 0.80 },
-							{ label: "Excitement", y: 0.80 },
-							{ label: "Frustration", y: 0.40 },
-							{ label: "Stress", y: 0.40 },
-							{ label: "Relaxation", y: 0.80 }
-						]					
-					}			
-				);
-			}	
-		};
+var updateChart = function (paramChart , paramData) {
+//	var barColor, yVal;
+	var dps = new Array(6);		
+	var measurements = ["Engagement","Focus","Excitement","Frustration","Stress","Relaxation"];		
+	console.log("I am here2");
+
+	for (var i=0; i< numUsers; i++){
+//		dps[i] = chart.options.data[i].dataPoints;
+		var name = paramChart.options.data[i].name;
 		
-	}
-	
-	var updateData = function(resource, secondsAgo, callback) {
-	    // Fetch data from our data provider
-	    provider.getData(resource, secondsAgo, function(newData) {
-	    	
-//	    	str = JSON.stringify(newData);
-//	        console.log(str);
-//	        alert(str);
-	        
-	      // Store the data locally
-	      data.addNewData(newData);
-	    	str = JSON.stringify(data);
-	        console.log(str);
-	        alert(str);
-	        
-//	      
-//	      // Remove data that's outside the window of data we are displaying. This
-//	      // is unnecessary to keep around.
-//	      data.removeDataOlderThan((new Date()).getTime()
-//	          - (graph.getTotalDurationToGraphInSeconds() * 1000));
-	      if (callback) {
-	        callback();
-	      }
-	    });
-	}
-	
-	var	updateChart = function(){
-		updateData(resource, secondsAgo);
-		chart.update(data);
-	}
-	
-	return {
-		
-		start: function() {
-			updateUserNum();
-			updateData(resource, secondsAgo, function(){
-				updateChart();				
-			});
+		for (var j=0; j< numUsers; j++){
+			if (paramData[j].name == name) {
+				paramChart.options.data[i].dataPoints = paramData[j].userData;
+			}				
 		}
 	}
-};
+	paramChart.render();			
+}
+
 
 /**
  * Provides access to records data.
@@ -208,7 +146,8 @@ var MeasurementDataProvider = function() {
  */
 var MeasurementData = function() {
 
-  var data = [];  
+  var data = [];
+  var dataPerUser={name:{}, userData:[]};
 
 //	var data={};
 	
@@ -229,10 +168,8 @@ var MeasurementData = function() {
 
     	newMeasurementData.forEach(function(record) {
     		
-    		var dataPerUser={name:record.host, userData:[]};
-//    		var user= record.host;
-//    		dataPerUser.name = record.host;
-    		
+    		var user= record.host;
+
 	        // Add individual measurement
 	        record.values.forEach(function(measurementValue) {
 	          // create a new data series entry for this measurement
@@ -243,50 +180,24 @@ var MeasurementData = function() {
 		          };
 	          
 	          // Update the measurement data
-	          
+	          dataPerUser.name = record.host;
 	          dataPerUser.userData.push(measureData);
 //	          data[j].push(measureData);
 	        });
-	        data.push(dataPerUser);	        
-      });
+	        data.push(dataPerUser);	
+	        
+      });    	
 //    	str = JSON.stringify(data);
 //        console.log(str);
 //        alert(str);    	
     }
-
-    
-    // convert data to overview data required data format
-//    toChartData : function() {
-//    	
-//    	chartData = [];
-//    	$.each(data, function (host, values){
-//	    	$.each(data, function(measurement, measureData) {
-//	    		chartData.push(
-//	    			{
-//		    			label: measurement,
-//		    			y:{} 
-//	    			}
-//	    		);	
-//	    	});
-//    	}); 
-//    }
   }
 }
 
-
-
-setInterval(function() {
-	
-}, 1000);
-
-
-var numUsers = 2;
+//var uiHelper = new UIHelper(data, chart);
+var numUsers = 3;
 var resource = "EEG sensor";
 var secondsAgo = 1;
-var data = new MeasurementData();
+var data;
+var dataAll = new MeasurementData();
 var provider = new MeasurementDataProvider();
-
-
-
-
-}//window.onload closing parenthesis
