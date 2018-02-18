d926d8f719799f4ae0451e14fd900fa0f532b191
wangso <wangso@missouri.edu>
2018-02-18 00:17:19 -0600

    working on color thresholding on data values

--- src/main/static-content/wwwroot/overview.js
+++ src/main/static-content/wwwroot/overview.js
@@ -1,6 +1,6 @@
 window.onload= function(){
 
-	var numUsers = 6;
+	var numUsers;
 	var resource = "EEG sensor";
 	var secondsAgo = 1;
 //	var data=[{"name":"user1","userData":[{"label":"engagement","y":0.88},{"label":"focus","y":0.816},{"label":"excitement","y":0.713},{"label":"frustration","y":0.293},{"label":"stress","y":0.175},{"label":"relaxation","y":0.512}]},
@@ -22,9 +22,30 @@ window.onload= function(){
 			data:[]
 		});	
 		
+	var updateData = function(resource, secondsAgo, callback) {
+		console.log("I am here 1");
+
+		//var localData;
+	    // Fetch data from our data provider
+	    provider.getData(resource, secondsAgo, function(newData) {
+			console.log("I am here 1.1");
+
+	      // Store the data locally
+			dataAll.resetData();
+	    	dataAll.addNewData(newData);
+//	    	dataAll.removeDataOlderThan((new Date()).getTime() - 1000);
+	    	//data=dataAll.getData();
+//	    	str = JSON.stringify(data);
+//	        console.log(str);
+	    	updateNumBars(numUsers, chart);
+	        if (callback) {
+		       callback();
+	        }	      	  
+	    });	 	    
+	};
 
 	var updateNumBars = function (num, paramChart){
-		console.log("I am here 1");
+		console.log("I am here 2");
 
 		paramChart.options.data=[];
 		for (var i=1; i<num+1; i++) {
@@ -69,30 +90,7 @@ window.onload= function(){
 					}			
 				);
 			}	
-		};
-
-		//chart.render();
-	};
-	
-	var updateData = function(resource, secondsAgo, callback) {
-		console.log("I am here 2");
-
-		//var localData;
-	    // Fetch data from our data provider
-	    provider.getData(resource, secondsAgo, function(newData) {
-			console.log("I am here 2.1");
-
-	      // Store the data locally
-			dataAll.resetData();
-	    	dataAll.addNewData(newData);
-//	    	dataAll.removeDataOlderThan((new Date()).getTime() - 1000);
-	    	//data=dataAll.getData();
-	    	str = JSON.stringify(data);
-	        console.log(str);
-	        if (callback) {
-		       callback();
-	        }	      	  
-	    });	 	    
+		};		
 	};
 	
 	var updateChart = function (paramChart , paramData) {
@@ -101,18 +99,41 @@ window.onload= function(){
 		var measurements = ["Engagement","Focus","Excitement","Frustration","Stress","Relaxation"];		
 		console.log("I am here 3");
 	
-		str = JSON.stringify(paramData);
-        console.log(str);
+//		str = JSON.stringify(paramData);
+//        console.log(str);
 		
 		for (var i=0; i< numUsers; i++){
-	//		dps[i] = chart.options.data[i].dataPoints;
+			
+			dps[i] = chart.options.data[i].dataPoints;
+			
 			var name = paramChart.options.data[i].name;
 			
 	        for (var j=0; j< numUsers; j++){
+	        	
 				if (paramData[j].name == name) {
 					paramChart.options.data[i].dataPoints = paramData[j].userData;
-				}				
+				}
+	        }	
+			
+	        for (var k= 0; k< dps[i].length; k++){	        	
+	        	
+				if paramChart.options.data[i].dataPoints[k].y <= 0.0 {
+					paramChart.options.data[i].dataPoints[k].y = 0.01;
+				}
+				if paramChart.options.data[i].dataPoints[k].y >= 1.0 {
+					paramChart.options.data[i].dataPoints[k].y = 0.99;
+				}
+				
+				barColor = yVal > 0.8 ? "Red" : yVal >= 0.5 ? "Yellow" : yVal < 0.5 ? "Green" : null;
+		        if (j==0 || j==1 || j==2 || j==5){
+		        	barColor = yVal <0.7? "Red": chart.options.data[i].color;
+		        }
+		        else {
+		        	barColor = yVal > 0.5 ? "Red": chart.options.data[i].color;
+		        }
+				dps[i][j] = {label: measurements[j] , y: yVal, color: barColor};
 			}
+	        chart.options.data[i].dataPoints = dps[i];
 		}
 		paramChart.render();
 
@@ -182,10 +203,14 @@ window.onload= function(){
 	    addNewData : function(newMeasurementData) {
 			console.log("I am here 5.1");
 			
-	    	str = JSON.stringify(newMeasurementData);
-	        console.log(str);				
-	    	
+//	    	str = JSON.stringify(newMeasurementData);
+//	        console.log(str);				
+			var userSet = new Set();
+			
 	    	newMeasurementData.forEach(function(record) {
+	    		
+	    		userSet.add(record.host);
+	    		
 	    		dataPerUser={name:{}, resource:{}, timeStamp:{}, userData:[]};
 	        	dataPerUser.timeStamp = record.timeStamp;
 	        	dataPerUser.resource = record.resource;
@@ -208,7 +233,9 @@ window.onload= function(){
 		        
 		        data.push(dataPerUser);	
 		        
-	      });    	
+	      });   
+	    	numUsers= userSet.size;
+	    	
 //	    	str = JSON.stringify(data);
 //	        console.log(str);
 	    },
@@ -246,25 +273,43 @@ window.onload= function(){
 	
 	var dataAll = new MeasurementData();
 	var provider = new MeasurementDataProvider();
-	//var uiHelper = new UIHelper(data, chart);
-	updateNumBars(numUsers, chart); 
-	updateData(resource, secondsAgo, 
-			function(){
-		updateChart(chart, data);
-		});
-//	updateData(resource, secondsAgo);
-//	updateChart(chart, data);
 	
+	// this does not work as there is asynchronous issues
+//	updateData(resource, secondsAgo);
+//	updateNumBars(numUsers, chart);
+//	updateChart(chart, data);	
+//	setInterval(function() {
+//		updateData(resource, secondsAgo);
+//		updateNumBars(numUsers, chart);
+//		updateChart(chart, data);
+//	}, 1000);
+	
+	
+	// this is the correct way of doing it but not quite working so far	
+	updateData(resource, secondsAgo, function(){
+		updateChart(chart, data);
+    });	
 	setInterval(function() {
-			
-		updateNumBars(numUsers, chart);  	
-		updateData(resource, secondsAgo, 
-				function(){
+		updateData(resource, secondsAgo, function(){
 			updateChart(chart, data);
-			});
+	    });
 	}, 1000);
 	
-
+	// this has been working before adding dynamic numUsers variable
+//	updateNumBars(numUsers, chart); 
+//	updateData(resource, secondsAgo, 
+//			function(){
+//		updateChart(chart, data);
+//		});
+//	
+//	setInterval(function() {
+//			
+//		updateNumBars(numUsers, chart);  	
+//		updateData(resource, secondsAgo, 
+//				function(){
+//			updateChart(chart, data);
+//			});
+//	}, 1000);
 
 
 }//window.onload closing parenthesis
